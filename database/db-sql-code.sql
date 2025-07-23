db8b97a88dd0d45b2830a2ca5ccff0fdb480e1ba
MAHONRRIMENDEZ <97697808+MAHONRRIMENDEZ@users.noreply.github.com>
2025-07-22 22:34:46 -0600

    completed

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -2,7 +2,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO demo456;
+    OWNER TO demo3403402;
 
 -- Table structure for table "classification"
 CREATE TABLE public.classification (
