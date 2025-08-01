2d0df69cb067f58dc15e9ebe082c3fbb0dbd11b4
MAHONRRIMENDEZ <97697808+MAHONRRIMENDEZ@users.noreply.github.com>
2025-07-31 23:06:26 -0600

    DONE

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.set("layout", "./layouts/layout") // not at views root- es decir que las pla
 app.use(static)
 
 //Index route
-app.get("/", baseController.buildHome) // esta es la version actualizada
+app.get("/", utilities.handleErrors(baseController.buildHome))// esta es la version actualizada
 /*
 app.get("/", function(req, res){ // esta es la version antes de la alteracion en la tarea de w3 "MVC Getting started"
   res.render("index", {title: "Home"})
@@ -53,9 +53,10 @@ app.use(async (req, res, next) => {
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
+    message,
     nav
   })
 })
